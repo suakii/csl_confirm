6fa90e6b5ae76edc51fb0b3394097606545c823b
Jonghwa Park <suakii@gmail.com>
2022-11-03 19:04:30 +0900

    csv upload

--- common/views.py
+++ common/views.py
@@ -1,6 +1,12 @@
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from .forms import UserForm
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from csl.models import StudentInform
+from django.contrib.auth.models import User
+from django.shortcuts import render, get_object_or_404
 
 
 def signup(request):
@@ -16,3 +22,56 @@ def signup(request):
     else:
         form = UserForm()
     return render(request, 'common/signup.html', {'form': form})
+
+
+def upload(request):
+    data = {}
+    if "GET" == request.method:
+        return render(request, "common/upload.html", data)
+    # if not GET, then proceed
+    try:
+        csv_file = request.FILES["csv_file"]
+        if not csv_file.name.endswith('.csv'):
+            messages.error(request, 'File is not CSV type')
+            return HttpResponseRedirect(reverse("common:upload"))
+        # if file is too large, return
+        if csv_file.multiple_chunks():
+            messages.error(request, "Uploaded file is too big (%.2f MB)." % (csv_file.size / (1000 * 1000),))
+            return HttpResponseRedirect(reverse("common:upload"))
+
+        file_data = csv_file.read().decode("utf-8")
+
+        lines = file_data.split("\n")
+        # loop over the lines and save them in db. If error , store as string and then display
+
+        for line in lines[1:]:
+            fields = line.split(",")
+            try:
+                question = StudentInform.objects.filter(student_id=User.objects.get(username=fields[0]).pk)
+                question.delete()
+            except Exception as e:
+                pass
+            try:
+                form = StudentInform()
+                form.student_id = User.objects.get(username=fields[0]).pk
+                form.inform1 = fields[1]
+                form.inform2 = fields[2]
+                form.inform3 = fields[3]
+                form.inform4 = fields[4]
+                form.inform5 = fields[5]
+                form.inform6 = fields[6]
+                form.inform7 = fields[7]
+                form.inform8 = fields[8]
+                form.inform9 = fields[9]
+                form.inform10 = fields[10]
+                form.save()
+
+            except Exception as e:
+                print(e)
+                pass
+
+    except Exception as e:
+        messages.error(request, "Unable to upload file. " + repr(e))
+
+    return render(request, 'csl/csl_list.html')
+
